3dffa201af6157482aeea96703cd0f29b4d90838
Justice Mubayiwa <j.mubayiwa@gmail.com>
2021-09-25 22:30:31 -0600

    Add a down arrow svg below buttons

--- src/components/Section.js
+++ src/components/Section.js
@@ -8,14 +8,19 @@ function Section() {
         <h1>The Podcast</h1>
         <p>Listen on all platforms </p>
       </ItemText>
-      <ButtonGroup>
-        <LeftButton>
-          Latest Episode
-        </LeftButton>
-        <RightButton>
-          Archives
-        </RightButton>
-      </ButtonGroup>
+      <Buttons>
+        <ButtonGroup>
+          <LeftButton>
+            Latest Episode
+          </LeftButton>
+          <RightButton>
+            Archives
+          </RightButton>
+        </ButtonGroup>
+        <DownArrow src='/images/down-arrow.svg'/>
+      </Buttons>
+      
+      
 
     </Wrap>
   )
@@ -27,10 +32,17 @@ const Wrap = styled.div `
   width: 100vw;
   height: 100vh;
   background: red;
-  background-image: url('./images/mountains brown trees.jpg');
+  background-image: url('./images/war.jpg');
   background-repeat: no-repeat;
   background-size: cover;
   background-position: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  font-size: 12px;
+  cursor: pointer;
+
 `
 const ItemText = styled.div`
   padding-top: 15vh;
@@ -41,6 +53,7 @@ const ButtonGroup = styled.div`
 `
 const LeftButton = styled.div`
   background-color: rgba(23, 26, 32, 0.8);
+  background-blend-mode: difference;
   height: 40px;
   width: 256px;
   color: white;
@@ -51,7 +64,17 @@ const LeftButton = styled.div`
   opacity: 0.85;
   text-transform: uppercase;
   font-size: 12px;
+  margin: 20px;
 `
 const RightButton = styled(LeftButton)`
+
+`
+const DownArrow = styled.img`
+  height: 40px;
+  animation: animateDown infinite 1.5s;
+  overflow-x: hidden;
+`
+const Buttons = styled.div`
+
 
 `
