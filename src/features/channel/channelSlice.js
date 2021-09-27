008a116ba713860a2cba1badf86fc4050384bac8
Justice Mubayiwa <j.mubayiwa@gmail.com>
2021-09-27 02:45:16 -0600

    change history to politics

--- src/features/channel/channelSlice.js
+++ src/features/channel/channelSlice.js
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  channels: ['Podcasts','Technology','Sports','Music','History']
+  channels: ['Podcasts','Technology','Sports','Music','Politics']
 }
 
 const channelSlice = createSlice({
