1a7823d0900dfa001fed1013350bbdbf80a776cd
Justice Mubayiwa <j.mubayiwa@gmail.com>
2021-09-26 19:23:52 -0600

    ease menu in and out

--- src/components/Header.js
+++ src/components/Header.js
@@ -105,6 +105,7 @@ const BurgerNav = styled.div`
   flex-direction: column;
   text-align: start;
   transform: ${props => props.show ? 'translateX(0)': 'translateX(100%)'};
+  transition: transform 0.3s ease-in; 
   li {
     padding: 15px 0;
     border-bottom: 1px solid rgba(250, 250, 250, 0.2)
