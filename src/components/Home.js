1790fc988462654b6017c75b8ebd73cc35978c64
Justice Mubayiwa <j.mubayiwa@gmail.com>
2021-09-26 22:32:59 -0600

    change channel background images

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,41 +2,42 @@ import React from 'react'
 import styled from "styled-components"
 import Section from './Section'
 
+
 function Home() {
   return (
     <Container>
       <Section
-        title="TUF Podcast"
-        description = "Listen on all platforms"
-        backgroundImg='perfect sundown.jpg'
+        title="Sculture"
+        description = 'For the overwhelming minority'
+        backgroundImg='dark pyramids.jpg'
         rightBtnText='Latest Episode'
         leftBtnText='Archives'
       />
       <Section
         title="Technology"
         description = ""
-        backgroundImg='yellow cassette.jpg'
+        backgroundImg='astro.jpg'
         rightBtnText='Updates'
         leftBtnText='Commentary'
       />
       <Section 
         title="Sports"
         description = ""
-        backgroundImg='open field soccer.jpg'
+        backgroundImg='boxing dodge.jpg'
         rightBtnText='Podcasts'
         leftBtnText='Stats' 
       />
       <Section 
         title="Music"
         description = ""
-        backgroundImg='black headset yellow background.jpg'
+        backgroundImg='piano hands.jpg'
         rightBtnText='Radar'
         leftBtnText='Playlists' 
       />
       <Section
         title="History"
         description = ""
-        backgroundImg='war.jpg'
+        backgroundImg='dino.jpg'
         leftBtnText='Library'
       />
       
